76200587a0e2cf8f3a656418a9f60ccdddd45674
avinash1605 <avinash1605@gmail.com>
2019-02-28 03:10:20 +0530

    secondary commit
    code updated

--- code.py
+++ code.py
@@ -7,8 +7,6 @@ warnings.filterwarnings('ignore')
 
 #dataset import and spliting it into X and Y
 dataset = pd.read_csv('house_prices.csv')
-X= dataset.iloc[:,[0,2,3,6,7]].values
-Y=dataset.iloc[:,8].values
 
 #Removing Location Null
 dataset = dataset.dropna(axis=0, subset=['location']) #Removing "1" Row which is having location Null as it is irrelevant to have a record for predicting price which has no loacation
@@ -25,6 +23,11 @@ dataset['availability']=dataset['availability'].astype('str')
 dataset['location']=dataset['location'].astype('str')
 dataset['size']=dataset['size'].astype('str')
 dataset['society']=dataset['society'].astype('str')
+
+#splitting dataset
+X= dataset.iloc[:,[0,2,3,6,7]].values
+Y=dataset.iloc[:,8].values
+
 #LABEL ENCODING
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 labenc_X = LabelEncoder()
@@ -43,6 +46,7 @@ X = oneHotEnc.fit_transform(X).toarray()
 oneHotEnc = OneHotEncoder(categorical_features=[2])
 X = oneHotEnc.fit_transform(X).toarray()
 
+
 #Splitting data sets into training and testing sets
 from sklearn.cross_validation import train_test_split
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,train_size=0.8,random_state=0)#preferably random state value is used as 0
@@ -55,6 +59,7 @@ regressor.fit(X_train , Y_train)
 Y_pred_train = regressor.predict(X_train) 
 Y_pred = regressor.predict(X_test) 
 
+
 #evaluating accuracy with RMSE
 
 def rmse(Y_pred,Y_test):
@@ -63,4 +68,49 @@ def rmse(Y_pred,Y_test):
     return acc
 print ("accuracy on training set = ",rmse(Y_pred_train,Y_train))
 print ("accuracy on test set = ",rmse(Y_pred,Y_test))
+-------------------------------------------------------------------------------------
+dataset2 = pd.read_csv('Predicting-House-Prices-In-Bengaluru-Test-Data.csv')
+#Removing Location Null
+dataset2 = dataset2.dropna(axis=0, subset=['location']) #Removing "1" Row which is having location Null as it is irrelevant to have a record for predicting price which has no loacation
+
+#Imputing Nulls From Size With Mode
+dataset2['size'].fillna(dataset2['size'].mode()[0], inplace=True)
+dataset2['bath'].fillna(dataset2['bath'].mean(), inplace=True)
+dataset2['balcony'].fillna(dataset2['balcony'].mean(), inplace=True)
+
+#string conversion 
+
+dataset2['area_type']=dataset2['area_type'].astype('str')
+dataset2['availability']=dataset2['availability'].astype('str')
+dataset2['location']=dataset2['location'].astype('str')
+dataset2['size']=dataset2['size'].astype('str')
+dataset2['society']=dataset2['society'].astype('str')
+#spliting 
+X_new = dataset2.iloc[:,[0,2,3,6,7]].values
+
+Y_new=dataset2.iloc[:,8].values
+
+#LABEL ENCODING
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+labenc_X_new = LabelEncoder()
+X_new[0]=X_new[0].astype('str')
+X_new[1]=X_new[1].astype('str')
+X_new[2]=X_new[2].astype('str')
+X_new[:,0]=labenc_X_new.fit_transform(X_new[:,0])
+X_new[:,1]=labenc_X_new.fit_transform(X_new[:,1])
+X_new[:,2]=labenc_X_new.fit_transform(X_new[:,2])
+
+#one hot enc
+oneHotEnc = OneHotEncoder(categorical_features=[0])
+X_new = oneHotEnc.fit_transform(X_new).toarray()
+oneHotEnc = OneHotEncoder(categorical_features=[1])
+X_new = oneHotEnc.fit_transform(X_new).toarray()
+oneHotEnc = OneHotEncoder(categorical_features=[2])
+X_new = oneHotEnc.fit_transform(X_new).toarray()
+
+#predicting
+Y_pred_new = regressor.predict(X_new) 
+
+
+
 
